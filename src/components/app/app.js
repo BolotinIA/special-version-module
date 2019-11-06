dc46e355ec023d2e1cf90a08b1a2db2014c68752
Иван <ivan.rusia@mail.ru>
2019-11-06 13:10:35 +1000

    Reset 'this.ready' flag before start

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -24,6 +24,7 @@ export default class Application {
 
         Helper.getNodes().then(nodes => {
             this.nodes = nodes;
+            this.ready = false;
             this.start();
         });
     }
